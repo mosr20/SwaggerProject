7ae1815f57b8353fa08f381e9b706ce74509dc4c
moshe <mosr20@gmail.com>
2023-01-15 18:32:54 +0200

    FIX MAIN.PY

--- Utilities/Base.py
+++ Utilities/Base.py
@@ -1,6 +1,5 @@
-from CreataObject import create_object
 from Configuration.config_file import set_config
-
+from Utilities.CreataObject import create_object
 
 Create_object = create_object("id","category","name","photoUrls","tags","status")
 
